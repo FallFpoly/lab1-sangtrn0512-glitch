f224d4f4db01df9ddaba3962ebe47b6e95e20147
sang <sang@DESKTOP-VPOH70L>
2025-09-22 20:26:09 +0700

    nop bai 4

--- Lab1/src/main/java/com/mycompany/lab1/bai4.java
+++ Lab1/src/main/java/com/mycompany/lab1/bai4.java
@@ -13,7 +13,7 @@ import java.util.Scanner;
 public class bai4 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner (System.in);
-        System.out.println("nhap a :");
+        System.out.println("nhap aa :");
         double a = scanner.nextDouble();
         System.out.println("nhap b :");
         double b = scanner.nextDouble();
