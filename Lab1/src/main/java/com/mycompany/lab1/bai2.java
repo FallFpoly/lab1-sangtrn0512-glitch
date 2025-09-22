419b162dc6f60ce209ea47ae25e603b51faf895b
sang <sang@DESKTOP-VPOH70L>
2025-09-22 19:39:35 +0700

    nop lab 1 bai 2

--- Lab1/src/main/java/com/mycompany/lab1/bai2.java
+++ Lab1/src/main/java/com/mycompany/lab1/bai2.java
@@ -1,16 +1,26 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
 package com.mycompany.lab1;
+
+import java.util.Scanner;
 
 /**
  *
- * @author ICT
+ * @author sang
  */
 public class bai2 {
-
     public static void main(String[] args) {
-          // sinh viên code ở đây
+        Scanner scanner = new Scanner (System.in);
+        System.out.println("nhap chieu dai : ");
+        double chieudai = scanner.nextDouble();
+        System.out.println("nhap chieu rong : ");
+        double chieurong = scanner.nextDouble();
+        System.out.printf("chu vi: %f\n ",(chieudai+chieurong)*2);
+        System.out.printf("dien tich la : %f\n ",chieudai*chieurong);
+        System.out.printf("canh nho nhat la : %f \n",Math.min(chieudai,chieurong));
+        
     }
+    
 }
