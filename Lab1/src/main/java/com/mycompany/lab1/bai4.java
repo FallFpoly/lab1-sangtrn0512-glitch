1d9297bdcdd40986cbb2d77de9e90da621ba6990
sang <sang@DESKTOP-VPOH70L>
2025-09-22 20:15:58 +0700

    nop  lab 1

--- Lab1/src/main/java/com/mycompany/lab1/bai4.java
+++ Lab1/src/main/java/com/mycompany/lab1/bai4.java
@@ -4,13 +4,25 @@
 
 package com.mycompany.lab1;
 
+import java.util.Scanner;
+
 /**
  *
  * @author ICT
  */
 public class bai4 {
-
     public static void main(String[] args) {
-          // sinh viên code ở đây
+        Scanner scanner = new Scanner (System.in);
+        System.out.println("nhap a :");
+        double a = scanner.nextDouble();
+        System.out.println("nhap b :");
+        double b = scanner.nextDouble();
+        System.out.println("nhap c :");
+        double c = scanner.nextDouble();
+        double delta = Math.pow(b,2)-4*a*c;
+        System.out.println("Delta la :"+delta);
+        System.out.println("can delta la : "+ Math.sqrt(delta));
     }
+    
+           
 }
