1fac65cf3dd5a31c7b02e99e0959888f54d88a34
sang <sang@DESKTOP-VPOH70L>
2025-09-22 20:12:20 +0700

    nop bai 1 lab 1

--- Lab1/src/main/java/com/mycompany/lab1/bai1.java
+++ Lab1/src/main/java/com/mycompany/lab1/bai1.java
@@ -11,6 +11,6 @@ package com.mycompany.lab1;
 public class bai1 {
 
     public static void main(String[] args) {
-        // sinh viên code ở đây
+        System.out.println("toi ten la sang");
     }
 }
